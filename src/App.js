51f3fee792dfd397c4ec9a2519102e288ee2aea9
119wjw <119wjw@naver.com>
2020-11-19 00:13:27 +0900

    react-router 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Home, Test } from './inc';
+const URL = 'http://localhost:4000';
+
+const App = () => {
+	return (
+		<div className='App'>
+			<Route path='/' component={Home} exact />
+			<Route path='/test' component={Test} />
+		</div>
+	);
+};
 
 export default App;
