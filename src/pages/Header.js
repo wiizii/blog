295898980ff6cabb4ddd09a018bbaf9ab632f4c2
119wjw <119wjw@naver.com>
2020-12-29 17:03:25 +0900

    logo 수정

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,34 +1,26 @@
-import React from "react";
-import logo from "../assets/images/logo.png";
-import circle from "../assets/images/circle.png";
-import "../App.css";
+import React from 'react';
+import gear from '../assets/images/gear.png';
+import gear_small from '../assets/images/gear_small.png';
+import '../App.css';
 
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo_container">
-        <div className="logo">
-          <img src={logo} width="200" height="200" alt="logo" />
-        </div>
-        <div className="circle">
-          <img
-            className="rotate_img"
-            src={circle}
-            width="200"
-            height="200"
-            alt="circle"
-          />
-        </div>
-        <section className="header_box">
-          <div className="auth_container">
-            <span className="subTitle">Let's make something awesome!</span>
-            <br />
-            <span className="author">SUPPORTED BY BTT</span>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
+	return (
+		<div className='header'>
+			<div className='logo_container'>
+				<div className='rotate_img'>
+					<img className='rotate_gear' src={gear} width='120' height='120' alt='gear' />
+					<img className='rotate_gear_small' src={gear_small} width='80' height='80' alt='small_gear' />
+				</div>
+				<section className='header_box'>
+					<div className='auth_container'>
+						<span className='subTitle'>Let's make something awesome!</span>
+						<br />
+						<span className='author'>SUPPORTED BY BTT</span>
+					</div>
+				</section>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
