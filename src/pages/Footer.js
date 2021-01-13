c5e0c33d0c836b4e3163f590e2888da510498cd2
119wjw <119wjw@naver.com>
2021-01-13 21:24:30 +0900

    footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
+import github_logo from '../assets/images/GitHub-Mark-64px.png';
 
 const Footer = () => {
-  return <div className="footer">footer</div>;
+	return (
+		<div className='footer'>
+			<div className='get_in_touch'>
+				<h4>GET IN TOUCH</h4>
+				<a className='mail' href='mailto:119wjw@gmail.com'>
+					119wjw@gmail.com
+				</a>
+			</div>
+
+			<div className='social_link'>
+				<a href='https://github.com/119wjw' target='_blank' rel='noreferrer'>
+					<img src={github_logo} width='32' height='32' alt='github_logo' />
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
