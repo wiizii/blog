e2ec8ba897e6e44de9175c31f39fb7a814971a55
119wjw <119wjw@naver.com>
2021-04-09 09:59:36 +0900

    Add dummy data

--- src/pages/Contents.js
+++ src/pages/Contents.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Categories from '../components/Categories';
+import Dummy from './Contents/Dummy';
 
 const Contents = ({ match }) => {
 	const category = match.params.category || 'ALL';
 	return (
 		<>
 			<Categories />
+			<Dummy />
 		</>
 	);
 };
--- src/pages/Contents/Dummy.js
+++ src/pages/Contents/Dummy.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 import DUMMY from '../../assets/dummy';
+
+const Container = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+	column-gap: 4px;
+	margin-left: 120px;
+	margin-right: 120px;
+	grid-auto-flow: dense;
+`;
+
+const ImageContainer = styled.div``;
+
+const Image = styled.img`
+	width: 100%;
+	height: auto;
+	border-radius: 5px;
+`;
 
 const itemsToRender = [];
 for (let x in DUMMY) {
 	itemsToRender.push(
-		<div key={x} className='image_container'>
-			<img src={DUMMY[x]} alt='dummy' className='dummy_image'></img>
-		</div>
+		<ImageContainer key={x} className='image_container'>
+			<Image src={DUMMY[x]} alt='dummy' className='dummy_image' />
+		</ImageContainer>
 	);
 }
 
 const Dummy = () => {
-	return <div className='grid_container'>{itemsToRender}</div>;
+	return <Container>{itemsToRender}</Container>;
 };
 
 export default Dummy;
